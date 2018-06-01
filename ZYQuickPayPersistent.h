6f737c52598920891509dc1ca7bf5fec438acff8
wangcongcong <clarence@jollycorp.com>
2018-06-01 16:00:01 +0800

    注释

--- ZYQuickPayPersistent.h
+++ ZYQuickPayPersistent.h
@@ -5,12 +5,15 @@
 //  Created by Clarence on 2018/5/16.
 //  Copyright © 2018年 Clarence. All rights reserved.
 //
-//  1. 用于将用户 支付密码和 指纹domainState 存储于keychain.
-//  2. domainState属于非敏感信息, 这里之所以也将domainState存入keychain, 是为了使domainState的生命周期和支付密码保持一致.
-//  3. 这两个信息分别以不同的Dict去存储, 是因为 domainState读取和更新的频次会远高于密码的读取和更新, 分别存储可减少domainState相对频繁的keyChain操作对密码的影响.
-//  4. 由于domainState属于非敏感信息, 为了提高读取更新成功率, 这里会将domainState存储于keyChain的同时, 也会将其存入UserDefault进行持久化.
-//  5. 由于password属于敏感信息, 为了提高读取跟新成功率, 这里会将password存储于keyChain的同时, 也会将其放入缓存中.
-//  NOTE:  使用该类进行信息的读取 必须保证用户为登陆状态.
+
+/*
+  1. 用于将用户 支付密码和 指纹domainState 存储于keychain.
+  2. domainState属于非敏感信息, 这里之所以也将domainState存入keychain, 是为了使domainState的生命周期和支付密码保持一致.
+  3. 这两个信息分别以不同的Dict去存储, 是因为 domainState读取和更新的频次会远高于密码的读取和更新, 分别存储可减少domainState相对频繁的keyChain操作对密码的影响.
+  4. 由于domainState属于非敏感信息, 为了提高读取更新成功率, 这里会将domainState存储于keyChain的同时, 也会将其存入UserDefault进行持久化.
+  5. 由于password属于敏感信息, 为了提高读取跟新成功率, 这里会将password存储于keyChain的同时, 也会将其放入缓存中.
+  NOTE:  使用该类进行信息的读取 必须保证用户为登陆状态.
+*/
 
 #import <Foundation/Foundation.h>
 
